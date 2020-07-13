965fec92200a83ea0da31370e8031f86805ec083
Ilham Muhammad <ilhammrg@gmail.com>
2020-07-13 19:52:02 +0700

    update icon

--- src/assets/components/navbar.js
+++ src/assets/components/navbar.js
@@ -8,7 +8,7 @@ class Navbar extends HTMLElement {
         <div class="navbar-fixed">
             <nav>
                 <div class="nav-wrapper">
-                    <a href="#" class="brand-logo"><img class="trophy-logo" src="/assets/icons/icon-512x512.png" alt="trophy">My League</a>
+                    <a href="#" class="brand-logo"><img class="trophy-logo" src="assets/icons/icon-72x72.png" alt="trophy">My League</a>
                     <ul id="nav-mobile" class="right hide-on-med-and-down">
                         <li><a href="#premier-league">Premier League</a></li>
                         <li><a href="#primera-division">Primera Division</a></li>
