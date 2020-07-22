3c9acbbaab6892fcc4e2ded02f82c4eaa4c8264b
Ilham Muhammad <ilhammrg@gmail.com>
2020-07-22 16:24:47 +0700

    update

--- src/assets/data-source/get-league-data.js
+++ src/assets/data-source/get-league-data.js
@@ -3,6 +3,16 @@ import fallbackComponents from "../components/fallback.component";
 const tokenAPI = '0d08af0570034b03a525c4af84529e5c';
 
 const getLeagueData = url => {
+    if ('caches' in window) {
+        caches.match(url)
+            .then(response => {
+                if (response) {
+                    return response.json();
+                } else {
+                    return;
+                }
+            });
+    }
     return fetch(url, {
         method: 'GET',
         mode: 'cors',
@@ -17,6 +27,16 @@ const getLeagueData = url => {
 
 const getClubPromised = (url, clubId) => {
     return new Promise ((resolve, reject) => {
+        if ('caches' in window) {
+            caches.match(url+clubId)
+                .then(response => {
+                    if (response) {
+                        resolve(response.json());
+                    } else {
+                        return;
+                    }
+                });
+        }
         fetch(url+clubId, {
             method: 'GET',
             mode: 'cors',
